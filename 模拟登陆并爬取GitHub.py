950b67d2c8ef075913558afae2e5e0962fc40601
SoriveZJQ <1792575431@qq.com>
2018-08-14 19:37:21 +0800

    发送form-data

---
+++ 模拟登陆并爬取GitHub.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+import requests
+from lxml import etree
+
+
+class Login(object):
+    def __init__(self):
+        self.headers = {
+            'Referer': 'https://github.com',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko)'
+                         'Chrome/65.0.3325.162 Safari/537.36',
+            'Host': 'github.com'
+        }
+        self.login_url = 'https://github.com/login'
+        self.post_url = 'https://github.com/session'
+        self.logined_url = 'https://github.com/settings/profile'
+        self.session = requests.Session()
+
+    def token(self):
+        response = self.session.get(self.login_url, headers=self.headers)
+        selector = etree.HTML(response.text)
+        token = selector.xpath('//div/input[2]/@value')[0]
+        return token
+
+    def login(self, email, password):
+        post_data = {
+            'commit': 'Sign in',
+            'utf8': '✓',
+            'authenticity_token': self.token(),
+            'login': email,
+            'password': password
+        }
+
+        response = self.session.post(self.post_url, data=post_data, headers=self.headers)
+        if response.status_code == 200:
+            self.dynamics(response.text)
+
+        response = self.session.get(self.logined_url, headers=self.headers)
+        if response.status_code == 200:
+            self.profile(response.text)
